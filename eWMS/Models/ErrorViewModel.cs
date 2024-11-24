a08c53611a0840b42ce06b0fdede0451f1d9907f
le.huy <hualehuy1997@gmail.com>
2024-11-24 23:01:46 +0700

    fix code

---
+++ eWMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eWMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
